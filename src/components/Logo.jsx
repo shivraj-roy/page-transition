a474d73c4d4b75089c72ff3d1c2a6e655ac7d63f
shivraj-roy <shivrajroy78@gmail.com>
2025-10-09 15:45:13 +0530

    Add logo component and transition styles for animations...

---
+++ src/components/Logo.jsx
@@ -0,0 +1,22 @@
+function Logo() {
+   return (
+      <svg
+         xmlns="http://www.w3.org/2000/svg"
+         width="160"
+         height="160"
+         viewBox="0 0 160 160"
+         preserveAspectRatio="xMidYMid meet"
+         fill="none"
+      >
+         <path
+            d="M446,182 L307,304 L423,312 L418,325 L370,329 L354,417 L493,297 L378,289 L384,274 L430,271 Z"
+            fill="none"
+            stroke="#e3e4d8"
+            strokeWidth="2"
+            strokeLinejoin="round"
+            strokeLinecap="round"
+         />
+      </svg>
+   );
+}
+export default Logo;
